a97a78e0b461f94dd1e7240600cc21eb0d17210f
sti- <cristi.naftanaila@gmail.com>
2015-03-16 21:39:44 +0200

    basic html css

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by STI on 3/16/2015.
+ */
+var express = require('express'),
+    mongoose = require('mongoose');
+
+var uristring = process.env.MONGOLAB_URI || 'mongodb://localhost/test';
+
+var app = express();
+
+mongoose.connect(uristring);
+
+app.use(express.static(__dirname + '/assets'));
+
+app.listen(process.env.PORT || 5000);
